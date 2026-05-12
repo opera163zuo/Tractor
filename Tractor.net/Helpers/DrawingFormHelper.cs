168d8f1352ce6706336f9c795db2c600efc80b8b
opera163zuo <opera163@gmail.com>
2026-05-12 17:26:33 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is behind 'origin/master' by 2 commits, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   Tractor.net/Helpers/DrawingFormHelper.cs
    	modified:   Tractor.net/bin/Debug/Kuaff.Tractor.exe
    	modified:   Tractor.net/bin/Debug/Kuaff.Tractor.pdb
    	modified:   Tractor.net/obj/Debug/Kuaff.Tractor.exe
    	modified:   Tractor.net/obj/Debug/Kuaff.Tractor.pdb

--- Tractor.net/Helpers/DrawingFormHelper.cs
+++ Tractor.net/Helpers/DrawingFormHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -161,8 +161,6 @@ namespace Kuaff.Tractor
                 }
             }
         }
-
-        private void DrawSuitCards(Graphics g)
 
         private void DrawSuitCards(Graphics g)
         {
